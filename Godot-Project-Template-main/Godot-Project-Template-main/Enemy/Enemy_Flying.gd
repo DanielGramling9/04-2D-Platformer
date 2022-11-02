d25ec10c252b4744a8e429e2d89840effc7777fe
DanielGramling9 <112036402+DanielGramling9@users.noreply.github.com>
2022-11-01 21:20:36 -0400

    Re-Organized Assets
    All sprites.

--- Godot-Project-Template-main/Godot-Project-Template-main/Enemy/Enemy_Flying.gd
+++ Godot-Project-Template-main/Godot-Project-Template-main/Enemy/Enemy_Flying.gd
@@ -25,3 +25,4 @@ func _physics_process(_delta):
 func _on_Area2D_body_entered(body):
 	if body.name =="Player":
 		body.do_damage(damage)
+		queue_free()
--- Godot-Project-Template-main/Godot-Project-Template-main/Enemy/Enemy_Ground.gd
+++ Godot-Project-Template-main/Godot-Project-Template-main/Enemy/Enemy_Ground.gd
@@ -1,6 +1,9 @@
 extends KinematicBody2D
 export var speed= 2
 export var damage = 10
+#1 
+export var max_constraint =100
+export var min_constraint =-100
 var direction = 1
 var velocity= Vector2.ZERO
 
@@ -10,6 +13,13 @@ func _physics_process(_delta):
 		$Sprite.flip_h = true
 	if direction > 0 and !$Sprite.flip_h:
 		$Sprite.flip_h = false
+	if direction > 0 and position.x > max_constraint:
+		velocity.x = 0
+		direction = -1
+	if direction < 0 and position.x > min_constraint:
+		velocity.x = 0
+		direction = 1
+	
 	velocity.x += direction * speed
 	move_and_slide_with_snap(velocity, Vector2.UP)
 	for i in get_slide_count():
