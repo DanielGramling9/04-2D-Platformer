5a5b0429f2015fb335bb606aec0f9e02063bffa8
DanielGramling9 <112036402+DanielGramling9@users.noreply.github.com>
2022-11-02 16:54:35 -0400

    h

---
+++ Final/Final/Enemy/Enemy_Ground.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+export var speed= 2
+export var damage = 10
+#1 
+export var max_constraint =100
+export var min_constraint =-100
+var direction = 1
+var velocity= Vector2.ZERO
+
+
+func _physics_process(_delta):
+	if direction < 0 and !$Sprite.flip_h:
+		$Sprite.flip_h = true
+	if direction > 0 and !$Sprite.flip_h:
+		$Sprite.flip_h = false
+	if direction > 0 and position.x > max_constraint:
+		velocity.x = 0
+		direction = -1
+	if direction < 0 and position.x > min_constraint:
+		velocity.x = 0
+		direction = 1
+
+	velocity.x += direction * speed
+	move_and_slide_with_snap(velocity, Vector2.UP)
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		print(collision.collider.name)
+		if collision.collider.name == "Platform" or collision.collider.name == "Ground":
+			velocity.x = 0
+			direction *= -1
+		if collision.collider.name == "Player":
+			collision.collider.do_damage(damage)
+			
+			
+	
+
+
+
+#func _on_Area2D_body_entered(body):
+	#if velocity.x != 0 and (body.name =="Platform" or body.name == "Ground"):
+		#	velocity.x =0
+		#	direction*=-1
+		#	if body.name == "Player":
+		#		body.do_damage(damage)
---
+++ Final/Final/Levels/Portal.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+
+func _ready():
+	pass
+
+
+func _on_Portal_entered(body):
+	if body.name == "Player":
+		if name == "Portal_to_2":
+			Global.level = 2
+			get_tree().change_scene("res://Levels/Level2.tscn")
